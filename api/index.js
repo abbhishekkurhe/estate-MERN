86d3620fe1f877f0c4595006f57c7cfd4580f4a5
abbhishek kurhe <connectabbhishekkurhe@gmail.com>
2025-02-19 13:28:25 +0530

    create a middleware and functiion possible error

--- api/index.js
+++ api/index.js
@@ -1,20 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';                                  
-import dotenv from 'dotenv';
-import router from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO)
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-        process.exit(1);  // Stop the server on DB connection failure
-    });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Stop the server on DB connection failure
+  });
 
 const app = express();
 
@@ -22,16 +23,20 @@ const app = express();
 app.use(express.json());
 
 // Define routes before starting the server
-app.use('/api/user', router);
-app.use('/api/auth',authRouter);
+app.use("/api/user", router);
+app.use("/api/auth", authRouter);
 
-
-
-
-
-
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 // Start the server
 app.listen(3000, () => {
-    console.log("Server started on port 3000!!!");
+  console.log("Server started on port 3000!!!");
 });
