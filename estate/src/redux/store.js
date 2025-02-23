c778d3873b847d58222f178e38f2b54bb1ecac11
abbhishek k <connectabbhishekkurhe@gmail.com>
2025-02-23 11:13:01 +0530

    added redux perisit

--- estate/src/redux/store.js
+++ estate/src/redux/store.js
@@ -1,11 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/userSlice'; // Ensure default import
+import {persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore';
+
+const rootReducer = combineReducers({user:userReducer});
+
+const persistConfig={
+  key:'root',
+  storage,
+  version:1,
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer)
+
 
 export const store = configureStore({
-  reducer: { user: userReducer }, // No change needed here
+  reducer: persistedReducer, // No change needed here
   middleware: (getDefaultMiddleware) => // Fixed typo here
     getDefaultMiddleware({
       serializableCheck: false,
     }),
     devTools:true,
 });
+export const persistor = persistStore(store);
