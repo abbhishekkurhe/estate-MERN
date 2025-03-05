2ee9a0c51c87c6faffe0afc025a5744431e164fa
abbhishek k <connectabbhishekkurhe@gmail.com>
2025-03-05 09:25:05 +0530

    create listing api route

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import router from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-
+import listingRouter from './routes/listing.route.js'
 
 
 dotenv.config();
@@ -30,6 +30,8 @@ app.use(cookieParser());
 // Define routes before starting the server
 app.use("/api/user", router);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
